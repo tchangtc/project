c50f553b9eb93d87e5e1fa66ff1d3bb4c17994e1
unknown <547804088@qq.com>
2020-03-20 11:13:46 +0800

    led

--- server/index.js
+++ server/index.js
@@ -1,9 +1,38 @@
-const express = require("express");
-const app = express();
-app.get("/temp",(req,res) => {
-	res.send("24");
-})
+// Modules
+var Gpio = require('pigpio').Gpio;
+var express = require('express');
 
-app.listen(3000, () => {
-	console.log("Server listenin on port 3000")
-})
+// Express app
+var app = express();
+
+// Use public directory
+app.use(express.static('public'));
+
+// Create led instance
+var led = new Gpio(18, {mode: Gpio.OUTPUT});
+
+// Routes
+app.get('/', function (req, res) {
+
+  res.sendfile(__dirname + '/public/interface.html');
+
+});
+
+app.get('/set', function (req, res) {
+
+  // Set LED
+  dutyCycle = req.query.dutyCycle;
+  led.pwmWrite(dutyCycle);
+
+  // Answer
+  answer = {
+    dutyCycle: dutyCycle
+  };
+  res.json(answer);
+
+});
+
+// Start server
+app.listen(3000, function () {
+  console.log('Raspberry Pi Zero LED control started!');
+});
